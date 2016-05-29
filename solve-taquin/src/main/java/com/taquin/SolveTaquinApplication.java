4c4597d226a90ffacc7de39eaefb613ecb094346
bermeom <bermeom@users.noreply.github.com>
2016-05-29 12:26:36 -0500

    first commit

---
+++ solve-taquin/src/main/java/com/taquin/SolveTaquinApplication.java
@@ -0,0 +1,12 @@
+package com.taquin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveTaquinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveTaquinApplication.class, args);
+	}
+}
